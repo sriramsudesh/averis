adb8609f15634a4fb0d55548d8cc3d6d590ee65e
Sudesh Krishnamoorthy <sudesh@Sudeshs-MacBook-Pro.local>
2017-03-28 16:01:17 +0800

    added loads of code

--- app.js
+++ app.js
@@ -12,14 +12,85 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+
 // create a new express server
 var app = express();
+
+var bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(bodyParser(urlencodedParser));
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+
+
+
+
+var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+var natural_language_understanding = new NaturalLanguageUnderstandingV1({
+  'username': '4408bb46-b9f9-440b-a236-bb5da0155dcf',
+  'password': '18itZWMTeSEf',
+  'version_date': '2017-02-27'
+});
+
+app.get('/', function(req, res) {
+  myresult = "";
+  res.render('index',myresult);
+});
+
+
+
+
+app.post('/analyze', function(req, res) {
+        var mytext = req.body.analyzeText;
+        console.log('1' + mytext);
+        var parameters = {
+          'text': mytext ,
+          'features': {
+            'entities': {
+              'emotion': true,
+              'sentiment': true,
+              'limit': 2
+            },
+            'keywords': {
+              'emotion': true,
+              'sentiment': true,
+              'limit': 2
+            }
+          }
+        }
+        var myresult = "";
+
+        analyzeUnderstanding(parameters, function(output){
+          myresult = output;
+          res.render('index',{myresult : myresult});
+          
+        })
+        
+});
+
+function  analyzeUnderstanding (parameters,callback) {
+    natural_language_understanding.analyze(parameters, function(err, response) {
+      if (err)
+        console.log('error:', err);
+      else
+        console.log(JSON.stringify(response, null, 2));
+      callback(JSON.stringify(response, null, 2));
+    });
+
+}
+
+
+
+
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
---
+++ views/test.js
@@ -0,0 +1,10 @@
+
+var myconstant = "Sudesh";
+
+var address = {
+
+    'name': myconstant,
+    'home1' :'Tampines'
+}
+
+console.log(address.home1);
